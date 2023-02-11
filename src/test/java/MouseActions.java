b2a58a2fa331bd1de4caa4239f14b9fde87789a0
suman_mathad <suman.mathad@gmail.com>
2023-02-11 14:18:06 +0530

    test order and alerts code updated

--- src/test/java/MouseActions.java
+++ src/test/java/MouseActions.java
@@ -1,28 +1,43 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MouseActions {
-    WebDriver driver;
-        String TestPage = "https://the-internet.herokuapp.com/";
+    WebDriver driver= WebDriverManager.chromedriver().avoidShutdownHook().create();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+    String TestPage = "https://the-internet.herokuapp.com/";
 
     @BeforeEach
     public void SetUp() {
-        driver = WebDriverManager.chromedriver().avoidShutdownHook().create();
+        driver.get(TestPage);
     }
 
     @Test
-    public void MouseActionsTest(){
-        driver.get(TestPage);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    @Order(1)
+    public void HoverTest() throws InterruptedException {
         Assert.assertTrue(driver.getTitle().equals("The Internet"));
+        driver.findElement(By.cssSelector("a[href='/hovers']")).click();
+        //to be updated
+        driver.close();
     }
 
+    @Test
+    @Order(2)
+    public void AlertsTest(){
+        driver.findElement(By.cssSelector("a[href='/javascript_alerts']")).click();
+        driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        Assert.assertTrue(alert.getText().equals("I am a JS Alert"));
+        driver.quit();
+    }
 
 }
